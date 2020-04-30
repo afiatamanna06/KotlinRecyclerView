f1ad053870edd19387e5b28b33e0a094a61d83af
afiatamanna06 <afiatamanna06@gmail.com>
2020-04-30 22:54:20 +0600

    Users added with adapter

--- app/src/main/java/com/example/kotlinrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/kotlinrecyclerview/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.kotlinrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayout.VERTICAL,false)
+
+        val users = ArrayList<User>()
+        users.add(User("How could I be so blind","So please I feel like I might disappear"))
+        users.add(User("How could I be so blind","So please I feel like I might disappear"))
+        users.add(User("How could I be so blind","So please I feel like I might disappear"))
+        users.add(User("How could I be so blind","So please I feel like I might disappear"))
+
+        val adapter = CustomAdapter(users)
+        recyclerView.adapter = adapter
     }
 }
